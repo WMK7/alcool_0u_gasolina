4525e32a368ce7588823d73cac34a7455641f37a
MK <micaelmm36@gmail.com>
2023-12-22 18:59:53 -0400

    Desenvolvendo codigo

--- app/src/main/java/com/example/alcool_0u_gasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcool_0u_gasolina/MainActivity.kt
@@ -23,5 +23,52 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inicialilzarComponentesInterface()
+        btnCalcular.setOnClickListener{
+            calcularMelhorPrece()
+        }
     }
+
+    private fun calcularMelhorPrece() {
+        val precoAlcool = editAlcool.text.toString()
+        val precoGasolina = editGasolina.text.toString()
+
+        val resultadoValidacao = validarCampos(precoAlcool,precoGasolina)
+
+        val precoAlcoolNumero = precoAlcool.toDouble()
+        val precoGasoliaNumero = precoGasolina.toDouble()
+        val resultado = precoAlcoolNumero / precoGasoliaNumero
+
+        if (resultado >= 0.7){
+            textResultado.text = "Nesse Preço melhor abastecer com Gasolina"
+        }else{
+            textResultado.text = "Nesse Preço melhor abastecer com Alcool"
+        }
+
+    }
+
+    private fun validarCampos(pAlcool: String, pGasolina: String): Boolean {
+        textInputAlcool.error = null
+        textInputGasolina.error = null
+        if (pAlcool.isEmpty()){
+            textInputAlcool.error = "Digite o Preço do Alcool"
+            return false
+        }else if (pGasolina.isEmpty()){
+            textInputGasolina.error = "Digite o Preço da Gasolina"
+            return false
+        }
+        return true
+    }
+
+    private fun inicialilzarComponentesInterface(){
+        textInputAlcool = findViewById(R.id.text_input_alcool)
+        editAlcool = findViewById(R.id.edit_alcool)
+
+        textInputGasolina = findViewById(R.id.text_input_gasolina)
+        editGasolina = findViewById(R.id.edit_gasolina)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+        textResultado =findViewById(R.id.text_resultado)
+    }
+
 }
